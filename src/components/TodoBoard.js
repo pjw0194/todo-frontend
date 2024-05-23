62f1fa817951be6dc47b45c3817cc391ff7a6801
pjw0194 <pjw019472@gmail.com>
2024-05-23 03:20:04 -0500

    Update all features of todo list

--- src/components/TodoBoard.js
+++ src/components/TodoBoard.js
@@ -1,13 +1,17 @@
 import React from "react";
+import TodoItem from "./TodoItem";
 
-const TodoBoard = () => {
-  return (
-    <div>
-      <h2>Todo List</h2>
-      {/* <TodoItem/> will be here once we get the todoList */}
-      <h2>There is no Item to show</h2>
-    </div>
-  );
+const TodoBoard = ({ todos, onDelete, onUpdate }) => {
+	return (
+		<div>
+			<h2>Todo List</h2>
+			{todos.length > 0 ? (
+				todos.map((item) => <TodoItem {...{ item, onDelete, onUpdate }} />)
+			) : (
+				<h2>There is no item to show</h2>
+			)}
+		</div>
+	);
 };
 
 export default TodoBoard;
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,21 +1,34 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 
-const TodoItem = () => {
-  return (
-    <Row>
-      <Col xs={12}>
-        <div className={`todo-item`}>
-          <div className="todo-content">밥먹기</div>
+const TodoItem = ({ item, onDelete, onUpdate }) => {
+	const handleDelet = async () => {
+		onDelete(item._id);
+	};
+	const handleUpdate = async () => {
+		onUpdate(item._id, item.isComplete);
+	};
 
-          <div>
-            <button className="button-delete">삭제</button>
-            <button className="button-delete">끝남</button>
-          </div>
-        </div>
-      </Col>
-    </Row>
-  );
+	const itemClass = `todo-item ${item.isComplete ? "item-complete" : ""}`;
+
+	return (
+		<Row>
+			<Col xs={12}>
+				<div className={itemClass}>
+					<div className="todo-content">{item.task}</div>
+
+					<div>
+						<button className="button-delete" onClick={handleDelet}>
+							삭제
+						</button>
+						<button className="button-delete" onClick={handleUpdate}>
+							{item.isComplete ? "안끝남" : "끝남"}
+						</button>
+					</div>
+				</div>
+			</Col>
+		</Row>
+	);
 };
 
 export default TodoItem;
